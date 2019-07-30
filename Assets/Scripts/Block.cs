17d9de8619010e368ea4fa875198fcb86eee5711
NaruStark <47173300+NaruStark@users.noreply.github.com>
2019-07-30 16:38:02 +0200

    Base upload
    uploading base for now.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    [SerializeField] AudioClip destroySound;
+
+    // Cached reference
+    Level level;
+    GameStatus gamestatus;
+
+    private void Start()
+    {
+        gamestatus = FindObjectOfType<GameStatus>();
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(gamestatus == null) { gamestatus = FindObjectOfType<GameStatus>(); }
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(destroySound, Camera.main.transform.position, 1);
+        Destroy(gameObject);
+        level.BlockDestroyed();
+        gamestatus.AddScore();
+    }
+}
